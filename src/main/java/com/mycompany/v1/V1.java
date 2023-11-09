dd4e3f054d76b46d4cfbf8fa6cd709af99ee8e30
almagpm <20030274@itcelaya.edu.mx>
2023-11-08 18:34:13 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/v1/V1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.v1;
+
+/**
+ *
+ * @author COMPUTOCKS
+ */
+public class V1 {
+
+    public static void main(String[] args) {
+        NewJFrame ven= new NewJFrame();
+        ven.setVisible(true);  
+    }
+}
